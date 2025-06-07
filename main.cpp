cc921cd289bf9850ffa921b1af2ddb5a7e53b590
weekinvis <89652278+weekinvis@users.noreply.github.com>
2025-06-07 16:00:46 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using std::cout; using std::cin;
 int main()
 {
     char continuar = 's';
+    
     while(continuar == 's' || continuar == 'S') {
         jogo novojogo;
         cout << "\nDeseja Continuar? (s ou S)\n";
